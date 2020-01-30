33382e1cf10cea83ab75d1bd335e34a4ef3aabca
Miguel Rosillo <miguel.rosillo@yaydoo.com>
2020-01-30 11:41:41 -0600

    method to retrieve the messages sent to server to the app implemented, and method to render them in a map list implemented to in chat js conponent

--- App/Components/Chat.js
+++ App/Components/Chat.js
@@ -5,17 +5,25 @@ import io from "socket.io-client";
 
 export default function Chat() {
   const [messageToSend, setMessageToSend] = useState('');
+  const [receivedMessage, setReceivedMessage] = useState([]);
   const socket = useRef(null); // so we can access variable socket in the whole component
   useEffect( () => { // learnig and practicing arrow functions es6
-    socket.current = io("http://192.168.0.3:3001")
+    socket.current = io("http://192.168.0.3:3001");
+    socket.current.on('message', message => {
+      setReceivedMessage(prevState => [...prevState, message]);// we take the prev state and add the new message
+    })
   },[])
   const sendMessage = () => {
       socket.current.emit('message', messageToSend)
       setMessageToSend('');//once the message is sent we set the state to empty string again
-  }
+  };
+  const displayReceivedMessages = receivedMessage.map(message => (
+    <Text key={message}>{message}</Text>
+  ))
   return (
     <View style={styles.container}>
       <Text>Share your ideas!</Text>
+      {displayReceivedMessages}
       <TextInput 
         value={messageToSend}
         onChangeText={text => setMessageToSend(text)}
