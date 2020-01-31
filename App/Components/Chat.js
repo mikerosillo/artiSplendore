49805fdb38cad980050763cb6dfe769547c2d0c2
Miguel Rosillo <miguel.rosillo@yaydoo.com>
2020-01-30 20:58:36 -0600

    chat view added to project

--- App/Components/Chat.js
+++ App/Components/Chat.js
@@ -1,3 +1,8 @@
+console.ignoredYellowBox = ['Remote debugger'];
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings([
+    'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+]);
 import React, { useEffect, useState, useRef } from 'react'; // learning and practicing hooks
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import io from "socket.io-client";
@@ -11,7 +16,7 @@ export default function Chat() {
     socket.current = io("http://192.168.0.3:3001");
     socket.current.on('message', message => {
       setReceivedMessage(prevState => [...prevState, message]);// we take the prev state and add the new message
-    })
+    });
   },[])
   const sendMessage = () => {
       socket.current.emit('message', messageToSend)
@@ -22,9 +27,10 @@ export default function Chat() {
   ))
   return (
     <View style={styles.container}>
-      <Text>Share your ideas!</Text>
+      <Text style={{fontSize:20, fontWeight:'500'}}>Share your ideas!</Text>
       {displayReceivedMessages}
       <TextInput 
+        style={{borderWidth:1, borderColor:'blue', width:'90%', borderRadius:4, marginTop:20, textAlign:'center'}}
         value={messageToSend}
         onChangeText={text => setMessageToSend(text)}
         placeholder='Enter message'
@@ -37,6 +43,7 @@ export default function Chat() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    minHeight:'100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
